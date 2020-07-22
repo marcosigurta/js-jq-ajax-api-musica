1b2fcade915bbc9c0aee9180c93df15737da153f
marco <marcosigurta.ovb@gmail.com>
2020-07-22 17:04:40 +0200

    Working

--- main.js
+++ main.js
@@ -32,13 +32,10 @@ function printAlbums(albums) {
 
     for (var i = 0; i < albums.length; i++) {
         var album = albums[i];
-        console.log(album)
+        var albumsHTML = compiled(album);
+        target.append(albumsHTML)
     }
 }
-
-
-
-
 
 function init() {
     ajaxCall();
